d1688c2f4c673eb8215efa7ad3d532576694c570
szymnob <65072795+szymnob@users.noreply.github.com>
2020-05-11 17:11:54 +0200

    Update textToExcel.py

--- Chapter 12/textToExcel.py
+++ Chapter 12/textToExcel.py
@@ -8,9 +8,10 @@ sheet = wb.active
 files = ['1.txt', '2.txt', '3.txt']
 
 for col in range(len(files)):
+    sheet.cell(column=col+1, row=1).value = files[col]
     lines = open(files[col], 'r').readlines()
     for rw in range(len(lines)):
-        sheet.cell(column=col+1, row=rw+1).value = lines[rw].strip('\n')
+        sheet.cell(column=col+1, row=rw+2).value = lines[rw].strip('\n')
 
 
 wb.save('atext.xlsx')
